eb8e3ccb7c2dd0a1ffd60feb9296c70679b6b3bc
Mehemmed898 <114071840+Mehemmed898@users.noreply.github.com>
2022-11-22 15:32:17 +0400

    Add files via upload

---
+++ src/Componento/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Price from './Price'
+import ProductName from './ProductName'
+
+const Product = () => {
+  return (
+    <>
+        <ProductName/>
+        <Price/>
+    </>
+  )
+}
+
+export default Product
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Product from './Componento/Product'
+import Componento from './componento/Product'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+<Product/>
+</>
+)
